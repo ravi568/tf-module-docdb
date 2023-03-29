9a6ce5bbc832013c55b091678f42dbf8aff3d422
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-03-29 23:09:27 +0530

    components to docdb, elasticache etcc..

--- main.tf
+++ main.tf
@@ -60,19 +60,19 @@ resource "aws_docdb_subnet_group" "main" {
 
 resource "aws_ssm_parameter" "docdb_url_catalogue" {
   name  = "${var.env}.docdb.url.catalogue"
-  type = "string"
+  type = "String"
   value = "mongodb://${data.aws_ssm_parameter.user.value}:${data.aws_ssm_parameter.pass.value}>@dev-docdb.cluster-cnjqtl3tzpfr.us-east-1.docdb.amazonaws.com:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
 }
 
 resource "aws_ssm_parameter" "docdb_url_user" {
   name  = "${var.env}.docdb.url.user"
-  type = "string"
+  type = "String"
   value = "mongodb://${data.aws_ssm_parameter.user.value}:${data.aws_ssm_parameter.pass.value}>@dev-docdb.cluster-cnjqtl3tzpfr.us-east-1.docdb.amazonaws.com:27017/users?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
 }
 
 resource "aws_ssm_parameter" "docdb_endpoint" {
   name = "${var.env}.docdb.endpoint"
-  type = "string"
+  type = "String"
   value = aws_docdb_cluster.main.endpoint
 
 }
